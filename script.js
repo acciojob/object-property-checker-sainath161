211c0127805d9450a376cea5141ac32fb01474f8
Sainath <71361447+sainath161@users.noreply.github.com>
2024-01-10 13:03:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	// Use the "in" operator to check if the key exists in the object
+  return key in sampleObject;
 }
 
 // Do not change the code below
